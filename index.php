4702c6427b3e41417dbadcd46a76fbbfebd5f6d8
Yurii Miroshnyk <yurii.miroshnyk@scandiweb.com>
2022-07-07 09:05:14 +0300

    PullRequest Test

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 session_start();
 
 ini_set('display_errors', 1);
-
+// This comment only for testing purpose
 require_once 'application/settings/main_config.php';
 require_once 'application/loader.php';
